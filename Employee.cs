5c053aaba59d70f4ec542d69f747873fa26c767c
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-03-28 13:00:03 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibrary
+{
+    //Employee "is a" /"is also a" Person 
+
+    //Person is parent class , Employee is child class
+    //Parent is referred in the child class using "base".
+    public class Employee:Person
+    {
+        public int Empid { get; set; }
+        public int Deptno { get; set; }
+
+        public Employee():base()
+        {
+            
+        }
+        public Employee(string fname,string lname,DateTime dob,int employeeid,int departmentno):base(fname,lname,dob)
+        {
+            //base.FirstName = fname;
+            //base.LastName = lname;
+            //base.DateOfBirth = dob;
+
+            Empid = employeeid;
+            Deptno = departmentno;
+            
+        }
+
+        public void DisplayEmpDetails()
+        {
+          base.DisplayEmpDetails();
+            Console.WriteLine(Empid);
+            Console.WriteLine(Deptno);
+        
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibrary
+{
+    public class Person
+    {
+        public Person()
+        {
+            FirstName = "Raj";
+           LastName = "Hareshwar";
+            DateOfBirth = new DateTime(1999, 09, 11);
+        }
+        public Person(string fname,string lname,DateTime dob)
+        {
+            FirstName=fname;
+            LastName=lname; 
+            DateOfBirth=dob;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+        public void DisplayEmpDetails()
+        {
+            Console.WriteLine(FirstName);
+            Console.WriteLine(LastName);
+            Console.WriteLine(DateOfBirth);
+
+        }
+
+    }
+}
