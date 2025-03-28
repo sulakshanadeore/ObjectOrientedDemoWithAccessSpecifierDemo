325b4d18a2c1b94945b98535dcf4d85cebd9e865
sulakshanadeore <deoresulakshana@gmail.com>
2025-03-28 12:58:28 +0530

    Constructor Inheritance

--- Program.cs
+++ Program.cs
@@ -9,59 +9,28 @@ internal class Program
         //Console.WriteLine("Etner Passwrod");
         //obj.Password = Console.ReadLine();
 
-        Console.WriteLine("Enter username");
-        string uname=Console.ReadLine();
-        Console.WriteLine("Enter password");
-        string pwd=Console.ReadLine();
-        Console.WriteLine("Enter mobile no");
-        long mobNo=Convert.ToInt64(Console.ReadLine());
+        //LoginAndPlaceOrder();
+       
+        Person p=new Person("Tina","Someone,",new DateTime(2000, 01, 11));
+        p.DisplayEmpDetails();
 
-        //Login obj = new Login("Gauri", "Gauri@123");
-        Login obj=new Login(mobNo,uname, pwd);
+        Person p1 = new Person();
+        p1.DisplayEmpDetails();
 
-        Console.WriteLine(obj.UserName + obj.Password + obj.MobileNo);
-
-        Console.WriteLine("Pls wait while we validate your credentials");
-        bool isCredentialsValid=obj.ValidateCredentials();
-        Console.WriteLine($"Enter this otp to validate yourself  {obj.OTP}");
-        Console.WriteLine("Enter the otp sent to you");
-        obj.CheckOTP=Convert.ToInt32(Console.ReadLine());
-        bool isloginSuccessful=obj.IsLoginSuccessful;
-        if (isloginSuccessful)
-        {
-            Console.WriteLine($"Your login is {isloginSuccessful}");
-            Products p=new Products();
-            p.ShowAllProducts();
-            Console.WriteLine("Now place your order");
-            Console.WriteLine("Enter productid");
-            p.ProductID=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter productname");
-            p.ProductName=Console.ReadLine();
-            Console.WriteLine("Enter Price");
-            p.Price=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter quantity");
-            p.Qty=Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Is the above order confirmed? Press Y/N...");
-            char userans=Convert.ToChar(Console.ReadLine());
-            if (userans == 'Y')
-            {
-                Orders custorder = new Orders(p.ProductID, p.ProductName, p.Price, p.Qty);
-                
-                double Amt = custorder.CalculateTotalPrice();
-                Console.WriteLine($"You have placed a order for {p.ProductName} for a Quantity of {p.Qty} At a price of Rs.{p.Price}, Total Price=Rs.{Amt}");
-
-
-            }
-            else {
-                Console.WriteLine("Thanks for visiting.....");
-            
-            
-            }
-
-        }
+        Console.WriteLine("-------------------------------------------");
+      //  Employee emp = new Employee("", "", new DateTime(), 123, 10);
+        Employee emp = new Employee("Paresh", "RRRR", new DateTime(2000, 01, 11), 123, 10);
+        //Person p=new Person();
+        //p.FirstName = "Tina";
+        //p.LastName = "SomeName";
+        //p.DateOfBirth = new DateTime(2000, 01, 11);
         
 
+        //Employee emp=new Employee();
+        emp.Empid = 123;
+        emp.Deptno = 10;
+        
+        emp.DisplayEmpDetails();
 
 
 
@@ -136,5 +105,61 @@ internal class Program
 
 
 
+    }
+
+    private static void LoginAndPlaceOrder()
+    {
+        Console.WriteLine("Enter username");
+        string uname = Console.ReadLine();
+        Console.WriteLine("Enter password");
+        string pwd = Console.ReadLine();
+        Console.WriteLine("Enter mobile no");
+        long mobNo = Convert.ToInt64(Console.ReadLine());
+
+        //Login obj = new Login("Gauri", "Gauri@123");
+        Login obj = new Login(mobNo, uname, pwd);
+
+        Console.WriteLine(obj.UserName + obj.Password + obj.MobileNo);
+
+        Console.WriteLine("Pls wait while we validate your credentials");
+        bool isCredentialsValid = obj.ValidateCredentials();
+        Console.WriteLine($"Enter this otp to validate yourself  {obj.OTP}");
+        Console.WriteLine("Enter the otp sent to you");
+        obj.CheckOTP = Convert.ToInt32(Console.ReadLine());
+        bool isloginSuccessful = obj.IsLoginSuccessful;
+        if (isloginSuccessful)
+        {
+            Console.WriteLine($"Your login is {isloginSuccessful}");
+            Products p = new Products();
+            p.ShowAllProducts();
+            Console.WriteLine("Now place your order");
+            Console.WriteLine("Enter productid");
+            p.ProductID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter productname");
+            p.ProductName = Console.ReadLine();
+            Console.WriteLine("Enter Price");
+            p.Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter quantity");
+            p.Qty = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Is the above order confirmed? Press Y/N...");
+            char userans = Convert.ToChar(Console.ReadLine());
+            if (userans == 'Y')
+            {
+                Orders custorder = new Orders(p.ProductID, p.ProductName, p.Price, p.Qty);
+
+                double Amt = custorder.CalculateTotalPrice();
+                Console.WriteLine($"You have placed a order for {p.ProductName} for a Quantity of {p.Qty} At a price of Rs.{p.Price}, Total Price=Rs.{Amt}");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Thanks for visiting.....");
+
+
+            }
+
+        }
     }
 }
