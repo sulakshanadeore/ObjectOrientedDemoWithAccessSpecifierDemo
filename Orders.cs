649839a7f86f3ce55504a913aa3b67407b84f084
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-03-28 12:11:34 +0530

    Add files via upload

---
+++ Orders.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLibrary
+{
+    public class Orders:Products
+    {
+       
+        public Orders(int productid, string productname, int price,int qty)
+        { 
+        
+            base.ProductID= productid;  
+            base.ProductName= productname;
+            base.Price= price;
+            base.Qty= qty;
+        
+
+        
+        }
+
+        
+        public double CalculateTotalPrice()
+        { 
+            return base.Qty *base.Price;
+          
+        }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,53 @@
+﻿namespace LogicLibrary
+{
+    public class Products
+    {
+        //Achieved encapsulation--by default all variables are private, ie accessible only inside the class
+        int _productid;//variable means field
+        string _productname;
+        float _price;
+        string _UnitOfMeasurement;
+        internal string _description;
+
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public  int Qty { get; set; }
+
+        public void ShowAllProducts()
+        {
+            Console.WriteLine("1. Tea 10 \n 2. Coffee 20");
+        
+        }
+
+        public void AcceptData(int productid, string productname, 
+            float price, string unitOfMeasurement)
+        {
+            
+            
+        _productid = productid;
+        _productname = productname;
+            _price = price;
+            _UnitOfMeasurement = unitOfMeasurement;
+        
+        }
+
+        public void DisplayData()
+        {
+           // Console.WriteLine(_productid + _productname + _price + _UnitOfMeasurement);
+           
+        string data=string.Concat(_productid, _productname,
+            _price, _UnitOfMeasurement);
+            Console.WriteLine(data);
+        
+        
+        }
+
+
+
+    }
+
+
+
+}
